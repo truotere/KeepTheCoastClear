56a20b3b83f5c23bdd0a06936d53cc89b1c10d1e
Teresa Truong <tetruong@ucsd.edu>
2021-05-16 21:49:55 -0700

    Set up basic interactive map with current location

---
+++ script.js
@@ -0,0 +1,26 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJ1b3RlcmUiLCJhIjoiY2tvbWhobDdwMDFnbTJucGR6bXc1NmliYiJ9.kGq6AG81Bp3I3Xw03qCKZw';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+});
+
+function successLocation(position) {
+    console.log(position);
+    setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+    setupMap([-117.212,32.76])
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 12
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+}
