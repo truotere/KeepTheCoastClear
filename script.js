2dd9be7e7fc086602592a915ba5aba9d93e3ff93
Teresa Truong <tetruong@ucsd.edu>
2021-05-22 16:40:32 -0700

    Complete outline of river and current data popup

--- script.js
+++ script.js
@@ -10,32 +10,31 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: apiKey
 }).addTo(mymap);
 
-// Marker
+/*Marker
 const marker = L.marker([32.76,-117.212]).addTo(mymap);
 
-// Popup
-let template = `
+// Marker Popup
+let markerPopup = `
     <h1>San Diego River</h1>
-    <img width="200" height="150" src="sdriver.jpeg"/>
+    <img width="500" height="400" src="current_data.png"/>
 `
-marker.bindPopup(template);
-
-/* Circle
-const circle = L.circle([32.76,-117.212], {
-    radius:500,
-    color: 'blue'
-}).addTo(mymap)
+marker.bindPopup(markerPopup);
 */
 
+// Polygon Popup
+let template = `
+    <div style="text-align:center">
+        <h1>San Diego River</h1>
+        <img width="600" height="450" src="current_data.png"/>
+    </div>
+`
 // River Polygon
 const polygon = L.polygon([
-
-    // bottom
 
     // Left Most Point
     [32.756333, -117.253590],
 
-    // top of polygon
+    // top of polygon (left to right)
     [32.761189, -117.212484],
     [32.762658, -117.207714],
     [32.762355, -117.203243],
@@ -61,44 +60,43 @@ const polygon = L.polygon([
     [32.795433, -117.097984],
     [32.796363, -117.097839],
 
-
     // Right Most Point
     [32.802096, -117.095154],
-
-    // bottom of polygon
-    [32.759520, -117.212484],//
-    [32.761658, -117.207714],
-    [32.761355, -117.203243],
-    [32.762366, -117.192636],
-    [32.761732, -117.190967],
-    [32.761835, -117.183377],
-    [32.761763, -117.181686],
-    [32.764334, -117.175528],
-    [32.764334, -117.173127],
-    [32.763999, -117.161028],
-    [32.771567, -117.149571],
-    [32.773341, -117.139968],
-    [32.774971, -117.131506],
-    [32.780587, -117.116885],
-    [32.781202, -117.112643],
-    [32.780387, -117.110727],
-    [32.780263, -117.106905],
-    [32.781610, -117.104901],
-    [32.783369, -117.104073],
-    [32.786556, -117.103525],
-    [32.789067, -117.103626],
-    [32.791333, -117.102261],
-    [32.794433, -117.097984],
+  
+    // bottom of polygon (right to left)
     [32.795363, -117.097839],
-
+    [32.795363, -117.097839],
+    [32.794433, -117.097984],
+    [32.791333, -117.102261],
+    [32.789067, -117.103626],
+    [32.786556, -117.103525],
+    [32.783369, -117.104073],
+    [32.781610, -117.104901],
+    [32.780263, -117.106905],
+    [32.780387, -117.110727],
+    [32.781202, -117.112643],
+    [32.780587, -117.116885],
+    [32.774971, -117.131506],
+    [32.773341, -117.139968],
+    [32.771567, -117.149571],
+    [32.763999, -117.161028],
+    [32.764334, -117.173127],
+    [32.764334, -117.175528],
+    [32.761763, -117.181686],
+    [32.761835, -117.183377],
+    [32.761732, -117.190967],
+    [32.762366, -117.192636],
+    [32.761355, -117.203243],
+    [32.761658, -117.207714],
+    [32.759520, -117.212484],
 ], {
     color: 'blue',
     fillColor: 'blue',
     fillOpacity:0.2
 }).addTo(mymap)
 
-polygon.bindPopup('cry me a river')
-
+//polygon.bindPopup('San Diego River')
+polygon.bindPopup(template);
 
 /*
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
