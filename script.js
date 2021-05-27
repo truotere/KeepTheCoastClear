808c97c16a54b93fbbf80d7e767126bbd18e7cac
Teresa Truong <tetruong@ucsd.edu>
2021-05-26 21:31:28 -0700

    Set up layers control

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // API Key Setup
 const apiKey = 'pk.eyJ1IjoiaXNhYWNnY3Jvc3Rod2FpdGUiLCJhIjoiY2tveHJoMHpkMGJqNTJvcjlkMjkwdWVyMSJ9.sKzywThwKm0SgIqc7IxV7AA';
 
-const mymap = L.map('map').setView([32.76,-117.212], 14);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/isaacgcrosthwaite/ckp2zja7g49rg17pk0cd9vfjd/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaXNhYWNnY3Jvc3Rod2FpdGUiLCJhIjoiY2tveHJoMHpkMGJqNTJvcjlkMjkwdWVyMSJ9.sKzywThwKm0SgIqc7IxV7A', {
+// Base Layer
+const mymap = L.map('map').setView([32.76,-117.212], 11);
+const base = L.tileLayer('https://api.mapbox.com/styles/v1/isaacgcrosthwaite/ckp2zja7g49rg17pk0cd9vfjd/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaXNhYWNnY3Jvc3Rod2FpdGUiLCJhIjoiY2tveHJoMHpkMGJqNTJvcjlkMjkwdWVyMSJ9.sKzywThwKm0SgIqc7IxV7A', {
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
@@ -11,7 +11,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/isaacgcrosthwaite/ckp2zja7g49rg17p
     accessToken: apiKey
 }).addTo(mymap);
 
-/* Impact Map Markers */
+
+
+
+/* IMPACT MAP LAYER */
 
 // Colored Marker Icons
 var LeafIcon = L.Icon.extend({
@@ -106,79 +109,85 @@ const lit5 = L.polygon([
 
 
 // Beach Cleanup
-L.marker([32.79629, -117.25667], {icon: blue}).addTo(mymap).bindPopup('<strong>Crystal Pier Beach Cleanup</strong><p>On January 26, 2019, Coastkeeper help a community beach cleanup at Crystal Pier in Pacific Beach. We engaged 147 volunteers who removed 4,151 items of trash, totalling 256 pounds.</p>');
+const beach1 = L.marker([32.79629, -117.25667], {icon: blue}).addTo(mymap).bindPopup('<strong>Crystal Pier Beach Cleanup</strong><p>On January 26, 2019, Coastkeeper help a community beach cleanup at Crystal Pier in Pacific Beach. We engaged 147 volunteers who removed 4,151 items of trash, totalling 256 pounds.</p>');
 //const beach1 = L.marker([32.79629, -117.25667]).addTo(mymap);
 //beach1.bindPopup('Crystal Pier Beach Cleanup');
 
-L.marker([32.961473, -117.268098], {icon: blue}).addTo(mymap).bindPopup('<strong>15th Street/Powerhouse Park Beach Cleanup</strong><p>On March 9, 2019, Coastkeeper help a community beach cleanup at Powerhouse Park in Del Mar. We engaged 193 volunteers who removed 2,763 items of trash, totalling 132.50 pounds.</p>');
+const beach2 = L.marker([32.961473, -117.268098], {icon: blue}).addTo(mymap).bindPopup('<strong>15th Street/Powerhouse Park Beach Cleanup</strong><p>On March 9, 2019, Coastkeeper help a community beach cleanup at Powerhouse Park in Del Mar. We engaged 193 volunteers who removed 2,763 items of trash, totalling 132.50 pounds.</p>');
 //const beach2 = L.marker([32.961473, -117.268098]).addTo(mymap);
 //beach2.bindPopup('15th Street/Powerhouse Park Beach Cleanup');
 
-L.marker([32.7825908, -117.2524943], {icon: blue}).addTo(mymap).bindPopup('<strong>Bank of America Beach Cleanup</strong><p>On April 5, 2019, Coastkeeper partnered with Bank of America through our beach cleanup in-a-box program. Four volunteers removed 128 items of trash, totalling five pounds, from Mission Beach.</p>');
+const beach3 = L.marker([32.7825908, -117.2524943], {icon: blue}).addTo(mymap).bindPopup('<strong>Bank of America Beach Cleanup</strong><p>On April 5, 2019, Coastkeeper partnered with Bank of America through our beach cleanup in-a-box program. Four volunteers removed 128 items of trash, totalling five pounds, from Mission Beach.</p>');
 //const beach3 = L.marker([32.7825908, -117.2524943]).addTo(mymap);
 //beach3.bindPopup('Bank of America Beach Cleanup');
 
-L.marker([32.8573693, -117.2535406], {icon: blue}).addTo(mymap).bindPopup('<strong>GKN Aerospace Beach Cleanup</strong><p>On April 18, 2019, Coastkeeper partnered with GKN Aerospace to co-host a beach cleanup at La Jolla Shores. Ten volunteers removed 235 items of trash, totalling 23 pounds of trash.</p>');
+const beach4 = L.marker([32.8573693, -117.2535406], {icon: blue}).addTo(mymap).bindPopup('<strong>GKN Aerospace Beach Cleanup</strong><p>On April 18, 2019, Coastkeeper partnered with GKN Aerospace to co-host a beach cleanup at La Jolla Shores. Ten volunteers removed 235 items of trash, totalling 23 pounds of trash.</p>');
 //const beach4 = L.marker([32.8573693, -117.2535406]).addTo(mymap);
 //beach4.bindPopup('GKN Aerospace Beach Cleanup');
 
 
 // Restoration Effort
-L.marker([32.5880126953125, -117.072227478027], {icon: yellow}).addTo(mymap).bindPopup('<strong>Otay Valley Regional Park</strong><p>On October 16, 2019, Coastkeeper and the park rangers at Otay Valley Regional Park hosted a resotration and cleanup event. Sixty-one volunteers removed 40 cubic yards of invasive plants and trash from the Otay Valley Regional Park.</p>');
+const restore1 = L.marker([32.5880126953125, -117.072227478027], {icon: yellow}).addTo(mymap).bindPopup('<strong>Otay Valley Regional Park</strong><p>On October 16, 2019, Coastkeeper and the park rangers at Otay Valley Regional Park hosted a resotration and cleanup event. Sixty-one volunteers removed 40 cubic yards of invasive plants and trash from the Otay Valley Regional Park.</p>');
 //const restore1 = L.marker([32.5880126953125, -117.072227478027]).addTo(mymap);
 //restore1.bindPopup('Otay Valley Regional Park');
 
 
 // Advocacy Win
-L.marker([32.8801756322311, -117.19894066569], {icon: orange}).addTo(mymap).bindPopup('<strong>Pure Water San Diego - North City Water Reclamation Plant</strong>');
+const advocacy1 = L.marker([32.8801756322311, -117.19894066569], {icon: orange}).addTo(mymap).bindPopup('<strong>Pure Water San Diego - North City Water Reclamation Plant</strong>');
 //const advocacy1 = L.marker([32.8801756322311, -117.19894066569]).addTo(mymap);
 //advocacy1.bindPopup('Pure Water San Diego - North City Water Reclamation Plant');
 
 
 // Water Quality Monitoring
-L.marker([32.687481, -117.1276321], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong><p>Chollas Creek runs through the historically disenfranchised community which shares its namesake, and is one of the most polluted waterbodies in San Diego. From 2009-2018, Coastkeeper ran the largest volunteer-driven ambient water quality monitoring program in the region, collecting monthly data reflecting the health of our waterways, including Chollas Creek, throughout San Diego County. The data collected from this project is currently being considered by California state officials to determine pollutant impairments for each waterbody.</p>');
+const monitor1 = L.marker([32.687481, -117.1276321], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong><p>Chollas Creek runs through the historically disenfranchised community which shares its namesake, and is one of the most polluted waterbodies in San Diego. From 2009-2018, Coastkeeper ran the largest volunteer-driven ambient water quality monitoring program in the region, collecting monthly data reflecting the health of our waterways, including Chollas Creek, throughout San Diego County. The data collected from this project is currently being considered by California state officials to determine pollutant impairments for each waterbody.</p>');
 //const monitor1 = L.marker([32.687481, -117.1276321]).addTo(mymap);
 //monitor1.bindPopup('Sampling Site');
 
-L.marker([32.69569, -117.1223602], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong>');
+const monitor2 = L.marker([32.69569, -117.1223602], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong>');
 //const monitor2 = L.marker([32.69569, -117.1223602]).addTo(mymap);
 //monitor2.bindPopup('Sampling Site');
 
-L.marker([32.71196, -117.1202316], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong>');
+const monitor3 = L.marker([32.71196, -117.1202316], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong>');
 //const monitor3 = L.marker([32.71196, -117.1202316]).addTo(mymap);
 //monitor3.bindPopup('Sampling Site');
 
-L.marker([32.72715, -117.0699463], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong>');
+const monitor4 = L.marker([32.72715, -117.0699463], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong>');
 //const monitor4 = L.marker([32.72715, -117.0699463]).addTo(mymap);
 //monitor4.bindPopup('Sampling Site');
 
-L.marker([32.691921, -117.1126404], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong>');
+const monitor5 = L.marker([32.691921, -117.1126404], {icon: red}).addTo(mymap).bindPopup('<strong>Sampling Site</strong>');
 //const monitor5 = L.marker([32.691921, -117.1126404]).addTo(mymap);
 //monitor5.bindPopup('Sampling Site');;
 
 
 // Education
-L.marker([32.92599187, -117.1496417], {icon: purple}).addTo(mymap).bindPopup('<strong>Walking Field Trip - Sandberg Elementary</strong>');
+const education1 = L.marker([32.92599187, -117.1496417], {icon: purple}).addTo(mymap).bindPopup('<strong>Walking Field Trip - Sandberg Elementary</strong>');
 //const education1 = L.marker([32.92599187, -117.1496417]).addTo(mymap);
 //education1.bindPopup('Walking Field Trip - Sandberg Elementary');
 
-L.marker([32.77573049, -117.1711135], {icon: purple}).addTo(mymap).bindPopup('<strong>Guided Lesson - Carson Elementary</strong>');
+const education2 = L.marker([32.77573049, -117.1711135], {icon: purple}).addTo(mymap).bindPopup('<strong>Guided Lesson - Carson Elementary</strong>');
 //const education2 = L.marker([32.77573049, -117.1711135]).addTo(mymap);
 //education2.bindPopup('Guided Lesson - Carson Elementary');
 
-L.marker([32.90662956, -117.1569916], {icon: purple}).addTo(mymap).bindPopup('<strong>Guided Lesson - Salk Elementary</strong>');
+const education3 = L.marker([32.90662956, -117.1569916], {icon: purple}).addTo(mymap).bindPopup('<strong>Guided Lesson - Salk Elementary</strong>');
 //const education3 = L.marker([32.90662956, -117.1569916]).addTo(mymap);
 //education3.bindPopup('Guided Lesson - Salk Elementary');
 
-L.marker([32.69188411, -117.109097], {icon: purple}).addTo(mymap).bindPopup('<strong>Science Talk - Chavez Elementary</strong>');
+const education4 = L.marker([32.69188411, -117.109097], {icon: purple}).addTo(mymap).bindPopup('<strong>Science Talk - Chavez Elementary</strong>');
 //const education4 = L.marker([32.69188411, -117.109097]).addTo(mymap);
 //education4.bindPopup('Science Talk - Chavez Elementary');
 
-L.marker([32.80795605, -117.1996296], {icon: purple}).addTo(mymap).bindPopup('<strong>Science Talk - Marston Middle School</strong>');
+const education5 = L.marker([32.80795605, -117.1996296], {icon: purple}).addTo(mymap).bindPopup('<strong>Science Talk - Marston Middle School</strong>');
 //const education5 = L.marker([32.80795605, -117.1996296]).addTo(mymap);
 //education5.bindPopup('Science Talk - Marston Middle School');
 
+// Layer Group: Impact Map
+const impactMap = L.layerGroup([lit1,lit2,lit3,lit4,lit5,beach1,beach2,beach3,beach4,restore1,advocacy1,monitor1,monitor2,monitor3,monitor4,monitor5,education1,education2,education3,education4,education5]);
 
+
+
+
+/* WATERBODIES LAYER */
 
 // River Polygon Popup
 let template = `
@@ -187,8 +196,8 @@ let template = `
         <img width="600" height="450" src="current_data.png"/>
     </div>
 `
-// River Polygon
-const polygon = L.polygon([
+// San Diego River Polygon
+const sdr = L.polygon([
 
     // Left Most Point
     [32.756333, -117.253590],
@@ -252,7 +261,23 @@ const polygon = L.polygon([
     color: 'blue',
     fillColor: 'blue',
     fillOpacity:0.2
-}).addTo(mymap)
+}).addTo(mymap).bindPopup(template);
 
-//polygon.bindPopup('San Diego River')
-polygon.bindPopup(template);
+// Layer Group: Waterbodies
+const waterbodies = L.layerGroup([sdr]);
+
+
+
+
+
+// Layers Control
+const baseMaps = {
+    "Base Layer": base
+};
+
+const overlayMaps = {
+    "Impact Map": impactMap,
+    "Waterbodies": waterbodies
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(mymap);
